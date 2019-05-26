d68e9b47ef815e950bc7199c6d335ad3515c7f05
Mateusz Zagrzejewski <mateusz.zagrzejewski@gmail.com>
2019-05-26 18:03:06 +0100

    kolejny commit

--- src/main/java/com/sda/algorytmy/App.java
+++ src/main/java/com/sda/algorytmy/App.java
@@ -1,13 +1,31 @@
 package com.sda.algorytmy;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
+    public static Path file = Paths.get("real_estate.csv");
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        try {
+            byte [] lines2 = Files.readAllBytes(file);
+            List<String> lines = Files.readAllLines(file);
+            System.out.println(lines.get(1));
+
+            for (String line: lines) {
+                System.out.println(line);
+            }
+        }catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 }
